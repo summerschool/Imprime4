3fee266728d00a34d6d24ca953ada673d59a4a43
Tiago Manuel Ferrão dos Santos <tiagomanuelfs@hotmail.com>
2014-07-18 12:15:38 +0100

    [INI][FIN] CONSEGUIMOS, IMPRIME 4 NA CONSOLA! Obrigado a todos pelo suporte!

---
+++ src/testpackage/Imprime4.java
@@ -0,0 +1,17 @@
+package testpackage;
+
+
+/**
+ * The Class Imprime4 is beautiful.
+ */
+public class Imprime4 {
+
+    
+    /**
+     * Imprime 4 no ecra.
+     */
+    public static void imprime() {
+        System.out.println(4);
+    }
+
+}
